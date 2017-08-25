3117f2812188b656fc5f934e1110d01f50c8299a
Tyler Veness <calcmogul@gmail.com>
2017-08-25 15:23:40 -0700

    Added tests for task.py functions

---
+++ wpiformat/test/test_task.py
@@ -0,0 +1,19 @@
+import os
+
+import wpiformat.task as task
+
+
+def test_task():
+    assert task.is_generated_file("." + os.sep + "wpiformat" + os.sep +
+                                  "wpiformat" + os.sep + "cpplint.py")
+    assert task.is_modifiable_file("." + os.sep + "wpiformat" + os.sep +
+                                   "cpplint.patch")
+    assert task.is_modifiable_file("." + os.sep + "wpiformat" + os.sep +
+                                   "javaguidelink.png")
+
+    assert not task.is_generated_file("." + os.sep + "wpiformat" + os.sep +
+                                      "diff_cpplint.py")
+    assert not task.is_generated_file("." + os.sep + "wpiformat" + os.sep +
+                                      "update_cpplint.py")
+    assert not task.is_modifiable_file("." + os.sep + "wpiformat" + os.sep +
+                                       "license.txt")
