ced8e7281c37cc4b89e9a776cf14f83f9881be2a
Tyler Veness <calcmogul@gmail.com>
2017-10-09 17:56:23 -0700

    Simplified newline.py implementation

--- wpiformat/wpiformat/newline.py
+++ wpiformat/wpiformat/newline.py
@@ -6,24 +6,9 @@ from wpiformat.task import Task
 class Newline(Task):
 
     def run_pipeline(self, config_file, name, lines):
-        linesep = Task.get_linesep(lines)
+        output = lines.rstrip() + Task.get_linesep(lines)
 
-        newlines = 0
-        pos = len(lines) - 1
-
-        # While last character is a newline
-        while pos >= 0 and lines[pos] == "\n":
-            newlines += 1
-
-            # Seek to character before newline
-            pos -= len(linesep)
-
-        if newlines < 1:
-            # Append newline to end of file
-            return (lines + linesep, True, True)
-        elif newlines > 1:
-            # Truncate all but one newline
-            return (lines[0:len(lines) - len(linesep) * (newlines - 1)], True,
-                    True)
+        if output != lines:
+            return (output, True, True)
         else:
             return (lines, False, True)
