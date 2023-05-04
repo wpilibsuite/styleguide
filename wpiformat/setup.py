58654505e51f471e741db1d976899df986e4ba03
Tyler Veness <calcmogul@gmail.com>
2023-05-03 19:45:59 -0700

    Upgrade black

--- wpiformat/setup.py
+++ wpiformat/setup.py
@@ -69,7 +69,7 @@ setup(
     zip_safe=True,
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
-    install_requires=["regex==2022.9.13", "black==22.10.0"],
+    install_requires=["regex==2022.9.13", "black==23.3.0"],
     license="BSD License",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
