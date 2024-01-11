6318dcba7a111e2faf4a931da5fd341c2f96e6cd
Thad House <ThadHouse@users.noreply.github.com>
2024-01-10 23:17:26 -0800

    Limit black workers to minimum of 60 or cpu count (#271)
    We can have more than 1, just not more than 60

--- wpiformat/wpiformat/pyformat.py
+++ wpiformat/wpiformat/pyformat.py
@@ -1,5 +1,6 @@
 """This task runs black on files with Python extension."""
 
+import multiprocessing as mp
 import subprocess
 import sys
 
@@ -14,10 +15,11 @@ class PyFormat(Task):
     @staticmethod
     def run_batch(config_file, names):
         try:
-            # Force one worker because wpiformat already uses a process pool,
+            # Force at max 60 workers because black uses multiprocessing,
             # and we need to keep the total number of multiprocessing processes
             # below the Windows system limit.
-            args = [sys.executable, "-m", "black", "--workers", "1", "-q"]
+            cpu_count = min(60, mp.cpu_count())
+            args = [sys.executable, "-m", "black", "--workers", str(cpu_count), "-q"]
             subprocess.run(args + names)
         except FileNotFoundError:
             print("Error: black not found in PATH. Is it installed?", file=sys.stderr)
