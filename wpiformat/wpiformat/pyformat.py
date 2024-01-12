46fe10b6d489f6ca82d1ded4f07d8776c7526e56
Tyler Veness <calcmogul@gmail.com>
2024-01-12 15:12:07 -0800

    Revert worker limits
    black and gersemi 0.11.0 now limit their internal process pools to 60.

--- wpiformat/wpiformat/pyformat.py
+++ wpiformat/wpiformat/pyformat.py
@@ -1,6 +1,5 @@
 """This task runs black on files with Python extension."""
 
-import multiprocessing as mp
 import subprocess
 import sys
 
@@ -15,11 +14,7 @@ class PyFormat(BatchTask):
     @staticmethod
     def run_batch(config_file, names):
         try:
-            # Force at max 60 workers because black uses multiprocessing,
-            # and we need to keep the total number of multiprocessing processes
-            # below the Windows system limit.
-            cpu_count = min(60, mp.cpu_count())
-            args = [sys.executable, "-m", "black", "--workers", str(cpu_count), "-q"]
+            args = [sys.executable, "-m", "black", "-q"]
             subprocess.run(args + names)
         except FileNotFoundError:
             print("Error: black not found in PATH. Is it installed?", file=sys.stderr)
