5ecdbd61b85c2039f856bc0508918670e37caeee
Tyler Veness <calcmogul@gmail.com>
2025-04-10 21:15:01 -0700

    Allow empty files in EOF newline task
    Per
    https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap03.html#tag_03_403,
    a file is allowed to have zero or more lines where a line is terminated
    by a newline character.

--- wpiformat/wpiformat/eofnewline.py
+++ wpiformat/wpiformat/eofnewline.py
@@ -1,8 +1,12 @@
-"""This task ensures that the file has exactly one EOF newline."""
+"""This task ensures that the file has zero EOF newlines if it's empty or one EOF newline."""
 
 from wpiformat.task import PipelineTask
 
 
 class EofNewline(PipelineTask):
     def run_pipeline(self, config_file, name, lines):
-        return lines.rstrip() + super().get_linesep(lines), True
+        lines = lines.rstrip()
+        if lines:
+            return lines + super().get_linesep(lines), True
+        else:
+            return lines, True
